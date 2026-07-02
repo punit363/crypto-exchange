2408b67087406a8edb4263006dda0e68febd81b6
Punit <3033.punit@gmail.com>
2026-07-02 16:52:58 +0530

    FEAT : new DB schema

---
+++ db/prisma/migrations/20260702112222_init/migration.sql
@@ -0,0 +1,121 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "order_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "side" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "quantity" DECIMAL(65,30) NOT NULL,
+    "filled_quantity" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "price" DECIMAL(65,30) NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'open',
+    "base_asset" TEXT NOT NULL,
+    "quote_asset" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("order_id")
+);
+
+-- CreateTable
+CREATE TABLE "trades" (
+    "trade_id" TEXT NOT NULL,
+    "taker_user_id" TEXT NOT NULL,
+    "maker_user_id" TEXT NOT NULL,
+    "taker_order_id" TEXT NOT NULL,
+    "maker_order_id" TEXT NOT NULL,
+    "quantity" DECIMAL(65,30) NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "base_asset" TEXT NOT NULL,
+    "quote_asset" TEXT NOT NULL,
+    "taker_side" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "trades_pkey" PRIMARY KEY ("trade_id")
+);
+
+-- CreateTable
+CREATE TABLE "candles" (
+    "candle_id" TEXT NOT NULL,
+    "interval" TEXT NOT NULL,
+    "base_asset" TEXT NOT NULL,
+    "quote_asset" TEXT NOT NULL,
+    "open" DECIMAL(65,30) NOT NULL,
+    "high" DECIMAL(65,30) NOT NULL,
+    "low" DECIMAL(65,30) NOT NULL,
+    "close" DECIMAL(65,30) NOT NULL,
+    "volume" DECIMAL(65,30) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "candles_pkey" PRIMARY KEY ("candle_id")
+);
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "tx_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "asset" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("tx_id")
+);
+
+-- CreateTable
+CREATE TABLE "balance_ledger" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "asset" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "type" TEXT NOT NULL,
+    "ref_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "balance_ledger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "candles_base_asset_quote_asset_interval_created_at_key" ON "candles"("base_asset", "quote_asset", "interval", "created_at");
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "trades" ADD CONSTRAINT "trades_taker_order_id_fkey" FOREIGN KEY ("taker_order_id") REFERENCES "orders"("order_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "trades" ADD CONSTRAINT "trades_maker_order_id_fkey" FOREIGN KEY ("maker_order_id") REFERENCES "orders"("order_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "trades" ADD CONSTRAINT "trades_taker_user_id_fkey" FOREIGN KEY ("taker_user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "trades" ADD CONSTRAINT "trades_maker_user_id_fkey" FOREIGN KEY ("maker_user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "balance_ledger" ADD CONSTRAINT "balance_ledger_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
